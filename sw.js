f39846fcaf3d7681dc412d5d054ccd9d5cb00be0
Anderson Martiniano <andersonmartiniano@gmail.com>
2018-05-01 20:12:49 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+var CACHE_NAME = 'js-tests-cache-v1';
+var urlsToCache = [
+  '/',
+  '/main.css',
+  '/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  console.log('install event');
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
