d1c6ad3f95a5715335f977ac61bcfc5a8a7ab067
Anderson Martiniano <andersonmartiniano@gmail.com>
2018-07-19 19:09:48 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
 var CACHE_NAME = 'js-tests-cache-v1';
 var urlsToCache = [
-  '/',
-  '/main.css',
-  '/main.js'
+  'main.css',
+  'main.js'
 ];
 
 self.addEventListener('install', function(event) {
